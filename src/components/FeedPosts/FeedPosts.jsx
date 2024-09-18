607d67c823d4b215115f48fb11e17cc685ee826f
terrxrmachine <so3.workspace@gmail.com>
2024-09-18 22:03:47 +0400

    поменял карточки товаров

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,55 +1,75 @@
-import {FormControl,
-	FormLabel, Divider,
-	Input, Box, Container, Flex, Skeleton, SkeletonCircle, Text, VStack, Image, CardBody, CardFooter, Button, Card, CardHeader, Avatar, Heading, IconButton, SimpleGrid, Stack } from "@chakra-ui/react";
-import FeedPost from "./FeedPost";
+import {
+  Box,
+  Text,
+  Flex,
+  VStack,
+  Skeleton,
+  SkeletonCircle,
+} from "@chakra-ui/react";
 import useGetFeedPosts from "../../hooks/useGetFeedPosts";
-import ContentCard from "./ContentCard";
-import { BiLike, BiChat, BiShare } from "react-icons/bi";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import useSearchUser from "../../hooks/useSearchUser";
-import { useRef } from "react";
+import ItemCard from "../ItemCard.jsx";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const FeedPosts = (data) => {
-	const { isLoading, posts } = useGetFeedPosts();
-	return (
-		<Container maxW={"container.xl"} py={5} px={2}>
-			{!isLoading && posts.length > 0 && posts.map((post) => <FeedPost key={post.id} post={post} />)}
-			{!isLoading && posts.length === 0 && (
-				<>
-					<Text fontSize={"md"} color={"red.400"}>
-						Dayuum. Looks like you don&apos;t have any friends.
-					</Text>
-				</>
-			)}
+const FeedPosts = () => {
+  const { isLoading, posts } = useGetFeedPosts();
 
-			{/*
-			<Flex gap='5'>
-				{isLoading &&
-					[0, 1, 2].map((_, idx) => (
-						<VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-							<Flex gap='2'>
-								<SkeletonCircle size='10' />
-								<VStack gap={2} alignItems={"flex-start"}>
-									<Skeleton height='10px' w={"200px"} />
-									<Skeleton height='10px' w={"200px"} />
-								</VStack>
-							</Flex>
-							<Skeleton w={"full"}>
-								<Box h={"200px"}>contents wrapped</Box>
-							</Skeleton>
-						</VStack>
-					))}
-			</Flex>
-			*/}
-
-			<ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1280: 3 }}>
-				<Masonry gutter="1rem">{data.posts.map((src) => (
-					<ContentCard linkImage={src} />
-				))}</Masonry>
-			</ResponsiveMasonry>
-		</Container>
-	);
+  return (
+    <Box width="100%">
+      {" "}
+      {/* Changed Container to Box with full width */}
+      {/* If loading is done and there are posts */}
+      {!isLoading && posts.length > 0 && (
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{
+            350: 2,
+            750: 2,
+            900: 3,
+            1200: 4,
+            1500: 5,
+            1800: 6,
+          }}
+        >
+          <Masonry gutter="1rem">
+            {posts.map((post) => (
+              <ItemCard
+                key={post.id}
+                imageUrl={post.imageURL}
+                title={post.title}
+                publishDate={post.publishDate}
+                likesNum={post.likesNum}
+                author={post.author}
+              />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+      )}
+      {/* If loading is done and there are no posts */}
+      {!isLoading && posts.length === 0 && (
+        <Text fontSize={"md"} color={"red.400"}>
+          Dayuum. Looks like you don&apos;t have any friends.
+        </Text>
+      )}
+      {/* If loading is ongoing */}
+      {isLoading && (
+        <Flex gap="5">
+          {[0, 1, 2].map((_, idx) => (
+            <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+              <Flex gap="2">
+                <SkeletonCircle size="10" />
+                <VStack gap={2} alignItems={"flex-start"}>
+                  <Skeleton height="10px" w={"200px"} />
+                  <Skeleton height="10px" w={"200px"} />
+                </VStack>
+              </Flex>
+              <Skeleton w={"full"}>
+                <Box h={"200px"}>contents wrapped</Box>
+              </Skeleton>
+            </VStack>
+          ))}
+        </Flex>
+      )}
+    </Box>
+  );
 };
 
 export default FeedPosts;
---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Box, Image, Text, Flex, IconButton, Avatar } from "@chakra-ui/react";
+
+const Like = "/Like.svg";
+const Favorite = "/Favorite.svg";
+const Comment = "/Comment.svg";
+const Send = "/Send.svg";
+const Dots = "/dots.svg";
+
+const ItemCard = ({ imageUrl, title, publishDate, likesNum, author }) => {
+  return (
+    <Box
+      width="300px"
+      backgroundColor="white"
+      border="1px solid #F0F6FD"
+      boxShadow="0px 10px 40px rgba(222, 230, 237, 0.4)"
+      borderRadius="12px"
+      overflow="hidden"
+      position="relative"
+      height="auto"
+    >
+      {/* Card Header */}
+      <Flex p="4" align="center">
+        <Avatar src={author.profilePicURL} size="sm" mr="3" />
+        <Box>
+          <Text fontSize="14px" fontWeight="400" color="#1B1D28">
+            {author.username}
+          </Text>
+          <Text fontSize="10px" fontWeight="400" color="#76777E">
+            {author.location}
+          </Text>
+        </Box>
+        <IconButton
+          icon={<Image src={Dots} alt="dots" />}
+          variant="ghost"
+          size="sm"
+          ml="auto"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          transform="rotate(90deg)" // Rotate dots to horizontal
+        />
+      </Flex>
+
+      {/* Card Image */}
+      <Image
+        src={imageUrl}
+        alt="Card Image"
+        width="100%"
+        objectFit="cover"
+        className="aspect-auto"
+      />
+
+      {/* Card Footer */}
+      <Box p="4">
+        {/* Action Icons */}
+        <Flex mb="2" justifyContent="space-between" alignItems="center">
+          <Flex>
+            <IconButton
+              icon={<Image src={Like} alt="Like" />}
+              variant="ghost"
+              aria-label="Like"
+            />
+            <IconButton
+              icon={<Image src={Comment} alt="Comment" />}
+              variant="ghost"
+              aria-label="Comment"
+            />
+            <IconButton
+              icon={<Image src={Send} alt="Send" />}
+              variant="ghost"
+              aria-label="Send"
+            />
+          </Flex>
+          <IconButton
+            icon={<Image src={Favorite} alt="Save" />}
+            variant="ghost"
+            aria-label="Save"
+          />
+        </Flex>
+
+        {/* Likes Text */}
+        <Text fontSize="12px" fontWeight="600" color="#1B1D28" mb="2">
+          Liked by <span className="font-bold">Jessica</span> and {likesNum}{" "}
+          others
+        </Text>
+
+        {/* Caption */}
+        <Text fontSize="12px" fontWeight="400" color="#1B1D28" mb="2">
+          &#8220; {title}... (More)
+        </Text>
+
+        {/* Date */}
+        <Text fontSize="10px" fontWeight="400" color="#76777E">
+          {publishDate}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default ItemCard;
